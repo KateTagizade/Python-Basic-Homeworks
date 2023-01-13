0b2c244470defa7b9c017387d517c1ec3ceef79a
KateTagizade <tagkate@gmail.com>
2023-01-13 21:51:40 +0200

    DZ 8

--- homework_07/growinghappy/services/models.py
+++ homework_07/growinghappy/services/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category (models.Model):
     name = models.CharField(max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name
+
 class Service (models.Model):
     name = models.CharField(max_length=64)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
